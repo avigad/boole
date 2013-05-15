fe32d4588e0075907589785101bdcd0001335181
Cody Roux <croux@cmu.edu>
2013-05-15 15:14:34 -0400

    A class for fresh variable generation

---
+++ boole/core/vargen.py
@@ -0,0 +1,46 @@
+#############################################################################
+#
+# vargen.py
+#
+# description: Fresh variable generation
+#
+#
+# Authors:
+# Cody Roux
+#
+#
+##############################################################################
+
+
+def inc_name(name, counts):
+    if name in counts:
+        counts[name] = counts[name] + 1
+    else:
+        counts[name] = 0
+
+
+class VarGen(object):
+    """Generate a fresh name according to a dictionary
+    sending names to a counter. These should never be reset.
+    """
+
+    def __init__(self):
+        """Initialize the index to dictionary.
+        """
+        self.default = '_Boole'
+        self._name_index = {}
+
+    def get_name(self, name=None):
+        """Return an unused name
+        """
+        if name != None:
+            pad = name
+        else:
+            pad = self.default
+        inc_name(pad, self._name_index)
+        i = self._name_index[pad]
+        if i == 0:
+            fresh = pad
+        else:
+            fresh = "{0!s}_{1!s}".format(pad, i)
+        return fresh
