e4a766c143b628c3240f9d94f8247923f19de8bc
Cody Roux <croux@cmu.edu>
2013-10-09 15:47:09 -0400

    Evaluation, end and semantics of 'built-in' functions. UNTESTED!

--- boole/semantics/value.py
+++ boole/semantics/value.py
@@ -82,6 +82,7 @@ class Model(object):
         """
         context.hyps.update(self.eqns)
 
+EmptyMod = Model({}, {})
 
 BoolVal = Value([True, False], 'Bool')
 
@@ -90,11 +91,11 @@ class ExprValue(e.ExprVisitor):
     """Return the value of an expression.
     """
     
-    def __init__(self, strict=False):
+    def __init__(self, strict):
         e.ExprVisitor.__init__(self)
         self.strict = strict
 
-    def visit_const(self, expr, model=None, bindings=None):
+    def visit_const(self, expr, model, bindings):
         if expr.value:
             return expr.value.pyval
         else:
@@ -107,74 +108,130 @@ class ExprValue(e.ExprVisitor):
                 else:
                     return None
 
-    def visit_db(self, expr, model=None):
+    def visit_db(self, expr, model, bindings):
+        if expr.index < len(bindings):
+            return bindings[-(expr.index + 1)]
         if self.strict:
             raise NoValue(expr)
         else:
             return None
 
-
-    def visit_type(self, expr, model=None):
+    def visit_type(self, expr, model, bindings):
         if self.strict:
             raise NoValue(expr)
         else:
             return None
 
-    def visit_kind(self, expr, model=None):
+    def visit_kind(self, expr, model, bindings):
         if self.strict:
             raise NoValue(expr)
         else:
             return None
 
-    def visit_bool(self, expr, model=None):
+    def visit_bool(self, expr, model, bindings):
         return BoolVal
 
-    def visit_bound(self, expr, model=None):
-        raise NotImplementedError()
+    def visit_bound(self, expr, model, bindings):
+        if expr.is_abst():
+            n_args = 0
+            e = expr
+            while e.is_abst():
+                n_args += 1
+                e = e.body
 
-    def visit_app(self, expr, model=None):
+            #A nameless function denoting the abstraction,
+            #note that arguments to nested lambdas are passed all at once.
+            def nameless(*args):
+                if len(args) > n_args:
+                    mess = "{0!s} arguments passed to a\
+                    function which takes only {1!s}"\
+                           .format(len(args), n_args)
+                    raise TypeError(mess)
+                else:
+                    return self.visit(e, model, bindings + args)
+                
+            return nameless
+
+        elif expr.is_forall():
+            dom_val = self.visit(expr.dom, model, bindings)
+            for v in dom_val:
+                if self.visit(expr.body, model, bindings + [v]):
+                    pass
+                else:
+                    return False
+            return True
+        
+        elif expr.is_exists():
+            dom_val = self.visit(expr.dom, model, bindings)
+            for v in dom_val:
+                if self.visit(expr.body, model, bindings + [v]):
+                    return True
+                else:
+                    pass
+            return False
+
+        elif self.strict:
+            raise NoValue(expr)
+        else:
+            return None
+
+    def visit_app(self, expr, model, bindings):
         #evaluate all the arguments at once.
         #Include the implicit arguments.
         f, args = e.root_app(expr)
-        f_val = self.visit(f, model)
-        args_val = [self.visit(a, model) for a in args]
+        f_val = self.visit(f, model, bindings)
+        args_val = [self.visit(a, model, bindings) for a in args]
         return f_val(*args_val)
 
-    def visit_pair(self, expr, model=None):
-        fst_val = self.visit(expr.fst, model)
-        snd_val = self.visit(expr.snd, model)
+    def visit_pair(self, expr, model, bindings):
+        fst_val = self.visit(expr.fst, model, bindings)
+        snd_val = self.visit(expr.snd, model, bindings)
         return (fst_val, snd_val)
 
-    def visit_fst(self, expr, model=None):
-        pair_val = self.visit(expr.expr, model)
+    def visit_fst(self, expr, model, bindings):
+        pair_val = self.visit(expr.expr, model, bindings)
         return pair_val[0]
 
-    def visit_snd(self, expr, model=None):
-        pair_val = self.visit(expr.expr, model)
+    def visit_snd(self, expr, model, bindings):
+        pair_val = self.visit(expr.expr, model, bindings)
         return pair_val[1]
 
-    def visit_ev(self, expr, model=None):
+    def visit_ev(self, expr, model, bindings):
         return ()
 
-    def visit_sub(self, expr, model=None):
-        def sub_val(v1, v2): 
-            if isinstance(v1, bool) and isinstance(v2, bool):
-                return (not v1) or v2
-            else:
-                return v1 == v2
-        return sub_val
+    def visit_sub(self, expr, model, bindings):
+        lhs_val = self.visit(expr.lhs, model, bindings)
+        rhs_val = self.visit(expr.rhs, model, bindings)
+        if isinstance(lhs_val, bool) and isinstance(rhs_val, bool):
+            return (not lhs_val) or rhs_val
+        else:
+            return lhs_val == rhs_val
 
-    def visit_box(self, expr, model=None):
-        return self.visit(expr.expr, model)
+    def visit_box(self, expr, model, bindings):
+        return self.visit(expr.expr, model, bindings)
 
-    def visit_tele(self, expr, model=None):
+    def visit_tele(self, expr, model, bindings):
         raise NotImplementedError()
 
-    def visit_mvar(self, expr, model=None):
+    def visit_mvar(self, expr, model, bindings):
         if self.strict:
             raise NoValue(expr)
         else:
             return None
+
+
+def eval_expr(expr, model=None, strict=False):
+    """Evaluate an expression in a model.
+    
+    Arguments:
+    - `expr`:
+    - `model`:
+    """
+    if model is None:
+        m = EmptyMod
+    else:
+        m = model
+    return ExprValue(strict).visit(expr, m, [])
 
 
 ###############################################################################
@@ -185,107 +242,134 @@ class ExprValue(e.ExprVisitor):
 
 def add_real_fun(x, y):
     return x + y
+add_real_val = Value(add_real_fun)
 
 
 def mul_real_fun(x, y):
     return x * y
+mul_real_val = Value(mul_real_fun)
 
 
 def minus_real_fun(x, y):
     return x - y
+minus_real_val = Value(minus_real_fun)
 
 
 def divide_real_fun(x, y):
     return float(x) / y
+divide_real_val = Value(divide_real_fun)
 
 
 def power_fun(x, y):
     return x ** y
+power_val = Value(power_fun)
 
 
 def uminus_real_fun(x):
     return -x
+uminus_real_val = Value(uminus_real_fun)
 
 
 def abs_real_fun(x):
     return abs(x)
+abs_real_val = Value(abs_real_fun)
 
 
 def lt_real_fun(x, y):
     return x < y
+lt_real_val = Value(lt_real_fun)
 
 
 def le_real_fun(x, y):
     return x <= y
+le_real_val = Value(le_real_fun)
 
 
 def add_int_fun(x, y):
     return x + y
+add_int_val = Value(add_int_fun)
 
 
 def mul_int_fun(x, y):
     return x * y
+mul_int_val = Value(mul_int_fun)
 
 
 def minus_int_fun(x, y):
     return x - y
+minus_int_val = Value(minus_int_fun)
 
 
 def divide_int_fun(x, y):
     return x / y
+divide_int_val = Value(divide_int_fun)
 
 
 def uminus_int_fun(x):
     return -x
+uminus_int_val = Value(uminus_int_fun)
 
 
 def abs_int_fun(x):
     return abs(x)
+abs_int_val = Value(abs_int_fun)
 
 
 def lt_int_fun(x, y):
     return x < y
+lt_int_val = Value(lt_int_fun)
 
 
 def le_int_fun(x, y):
     return x <= y
+le_int_val = Value(le_int_fun)
 
 
 def mod_fun(x, y):
     return x % y
+mod_val = Value(mod_fun)
 
 
 def eq_fun(_, x, y):
     return x == y
+eq_val = Value(eq_fun)
 
 
 def mul_fun(_, op, _1, x, y):
     return op(x, y)
+mul_val = Value(mul_fun)
 
 
 def add_fun(_, op, _1, x, y):
     return op(x, y)
+add_val = Value(add_fun)
 
 
 def minus_fun(_, op, _1, x, y):
     return op(x, y)
+minus_val = Value(minus_fun)
 
 
 def div_fun(_, op, _1, x, y):
     return op(x, y)
+div_val = Value(div_fun)
 
 
 def uminus_fun(_, op, _1, x):
     return op(x)
+uminus_val = Value(uminus_fun)
 
 
 def abs_fun(_, op, _1, x):
     return op(x)
+abs_val = Value(abs_fun)
 
 
 def lt_fun(_, op, _1, x, y):
     return op(x, y)
+lt_val = Value(lt_fun)
 
 
 def le_fun(_, op, _1, x, y):
     return op(x, y)
+le_val = Value(le_fun)
