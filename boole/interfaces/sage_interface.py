75cbe669ace6b3e082b6a8d44769b43f121ffd8f
Jeremy Avigad <avigad@cmu.edu>
2013-11-05 17:33:58 -0500

    added import

--- boole/interfaces/sage_interface.py
+++ boole/interfaces/sage_interface.py
@@ -21,6 +21,7 @@
 ###############################################################################
 
 from boole.elab.prelude import *
+from boole.elab.terms import *
 from boole.core.expr import open_bound_fresh_consts, ExprVisitor
 import boole.core.typing as ty
 import boole.core.tactics as tac
